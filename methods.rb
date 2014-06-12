efd899783f39c3252df14c92984b069a7356d9d8
Max Kohl <maxkohl810@gmail.com>
2014-06-12 10:34:28 -0400

    Max answers

--- methods.rb
+++ methods.rb
@@ -7,6 +7,7 @@ end
 y = square(2)
 # What is the value of 'x' after this line executes?
 
+x = nil because it is only defined within the square method.
 
 #### Question 2
 # What is the value of `age` after we call `get_older`?
@@ -18,6 +19,8 @@ end
 age = 10
 get_older(age)
 
+the value of age after get_older is run is 11. get_older passes in the age variable
+as an argument and adds 1 to itself.
 
 #### Question 3
 # What is the return value of `jump` when called on the last line here?
@@ -34,11 +37,17 @@ end
 energy = 2
 jump
 
+the return value of jump is 2
+
 
 #### Question 4
 # Write a method that accepts a string and an optional multiplier (defaults to 3)
 # and returns the original string multiplied that many times (using the * operator).
 # Do not use an options hash or keyword arguments.
+
+def string_multiplier(string, multiplier = 3)
+  string * multiplier
+end
 
 
 #### Question 5
@@ -48,3 +57,5 @@ def greeting(name:, salutation: 'Hi', volume: :low)
   greeting = "#{salutation} there, #{name}"
   volume == :high ? greeting.upcase : greeting
 end
+
+puts greeting(name: 'puny humans', salutation: 'hello', volume: 'high')
